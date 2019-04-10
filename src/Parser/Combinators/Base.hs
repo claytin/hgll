6750726f0212e7418449570de77abb02273e9d41
claytown <>
2019-04-10 18:14:58 -0300

    Back to old (list based) combinators

--- src/Parser/Combinators/Base.hs
+++ src/Parser/Combinators/Base.hs
@@ -1,35 +1,30 @@
--- TODO
--- Some of the comments for the implementation of some of the combinators do
--- not match their implementation
 module Parser.Combinators.Base ( Parser
-                               , PR(..)
+                               , ParseRes
                                , Input
                                , Label -- defined by Parser.Data.ParseTree
                                , eps
                                , term, t
                                , sqnc, s
-                               , alt, (>|<)) where
+                               , alt, (<|>) ) where
 
 import Parser.Data.ParseTree
 
 -- The result of applying a parser is either a Success, for which is given the
 -- correspondent ParseTree and the remainder of the input, or it is a Failure,
 -- for which we return the unmodified input
-data PR = Success ParseTree Input
-        | Failure Input
-
+type ParseRes = [(ParseTree, Input)]
 -- where
 type Input = String
 
-{-- Combinators --}
 -- A parser is a fuction that takes a memo, some data structure that holds
--- computed values, a string, and returns a PR
-type Parser = Input -> PR -- where
+-- computed values, a string, and returns a ParseRes
+type Parser = Input -> ParseRes
 
+{-- Combinators --}
 -- Parser that represents an empty production. The parser always succeeds with
 -- an "empty" ParseTree (see Parser.Data.Syntax)
 eps :: Parser
-eps = \i -> Success Eps i
+eps = \i -> [(Eps, i)]
 
 -- Since iso EBNF terminals are strings and that this implementation have no
 -- parametrization of its parsers, this is the only parser for matching
@@ -41,8 +36,8 @@ term s  = \i ->
     let n  = length s
         s' = take n i
         i' = drop n i
-    in if s == s' then Success (Leaf s') i'
-       else            Failure i
+    in if s == s' then [(Leaf s', i')]
+       else            []
 
 -- The sequence combinator takes a list of parsers and apply each of them, in
 -- order, to the given input. For each successful match the resulting ParseTree
@@ -50,42 +45,22 @@ term s  = \i ->
 -- correpondent sub-tree is retuned, otherwise it fails without modifiyng the
 -- original input, there is no partial parsing of a sequence
 sqnc      :: Label -> [Parser] -> Parser
-sqnc l [] = error $ "Sequence " ++ l ++ " is empty!"
-sqnc l ps = \i ->
-    let sqnc' [] inp acc     = (acc, inp)
-        sqnc' (q:qs) inp acc = case q inp of
-            (Success t r) -> sqnc' qs r (t:acc)
-            _             -> ([], inp)
-    in case sqnc' ps i [] of
-            ([], r) -> Failure r
-            (cs, r) -> Success (Node l (reverse cs)) r
+sqnc l [] = error $ "Sequence " ++ l ++ " must not be empty!"
+sqnc l ps = \i -> sqnc' ps i []
+    -- reverse is used because the parse trees are being prepended into acc
+    where sqnc' []     inp acc = [(Node l $ reverse acc, inp)]
+          sqnc' (q:qs) inp acc = case q inp of
+              []          -> []
+              [(t, inp')] -> sqnc' qs inp' (t:acc)
 
 -- A first match alternative combinator, if the first of the combinators (p)
 -- succeed then its result is returned, otherwise we try to match q
 alt     :: Parser -> Parser -> Parser
-alt p q = \i -> case p i of
-    v@(Success _ _) -> v
-    _               -> q i
+alt p q = \i -> p i ++ q i
 
--- Aliases
+{-- Aliases --}
 t = term
 s = sqnc
 
--- a >|< b >|< ... >|< z = a >|< (b >|< (... >|< z))
-infixr 2 >|< -- should this be left associative?
-(>|<) = alt
-
-instance Eq PR where
-    (Failure _)   == (Failure _)     = True
-    (Success _ _) == (Failure _)     = False
-    (Failure _)   == (Success _ _)   = False
-    (Success t r) == (Success t' r') = t == t' && r == r'
-
-{-- Pretty printing
- -- Don't worry about it, there is nothing pretty about pretty printing --}
-
-instance Show PR where
-    show (Success t r) = "Success _:" ++ show r ++ "\n"
-                      ++ "\n"
-                      ++ show t
-    show (Failure r)   = "Failure " ++ show r
+infixl 2 <|>
+(<|>) = alt
