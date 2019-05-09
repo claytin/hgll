4c29a771cdebb1e5e1b0accc295c9840229ce514
claytown <>
2019-05-09 16:36:10 -0300

    Added the set of combinators for the extend notation of the ISO EBNF

---
+++ src/Parser/Combinators/Ext.hs
@@ -0,0 +1,28 @@
+module Parser.Combinators.Ext ( opt 
+                              , clojure
+                              , times, (*.) ) where
+
+import Parser.Combinators.Base
+import Parser.Data.ParseTree
+
+opt   :: Parser ParseTree -> Parser ParseTree
+opt p = rule "Optional" [ p , eps ]
+
+clojure   :: Parser ParseTree -> Parser ParseTree
+clojure p = rule "Clojure" [ p', eps ]
+    where
+        p' = p         `bind` \x ->
+             clojure p `bind` \y ->
+             success $ Seq x y
+
+times     :: Int -> Parser ParseTree -> Parser ParseTree
+times 0 _ = eps
+times 1 p = p
+times n p = rule "Repetition" [ p' ]
+    where
+        p' = p `bind`                 \x ->
+             (n - 1) `times` p `bind` \y ->
+             success $ Seq x y
+
+infixl 0 *.  -- this may have to change
+(*.) = times
