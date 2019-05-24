25ae6a1e1a87283605b58a4df00eb6b79a31a6fc
claytown <>
2019-05-23 22:21:10 -0300

    Modified to work with ambiguous combinators

--- src/Parser/Generator.hs
+++ src/Parser/Generator.hs
@@ -7,11 +7,5 @@ import Parser.EBNF.Syntax
 
 gen   :: String -> Either String String
 gen i = case parse syntax i of
-    Right (t, remainder) ->
-        if length remainder > 0 then
-            Right $ gSyntax t
-        else
-            Left $ "Failed with a partial generation!\n"
-                ++ "Could not generate parsers for the following input:\n"
-                ++ remainder
-    Left i' -> Left $ "Parser failed at input:\n" ++ i'
+    [ ]      -> Left  $ "Parser failed. Aborting!\n"
+    [(t, i)] -> Right $ gSyntax t
