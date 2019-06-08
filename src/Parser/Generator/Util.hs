7ee5f60bb238cb0d3f7311e6fd3d8a87ff1671d1
claytown <>
2019-06-08 16:23:40 -0300

    gOpt now matches the structure of the other utility functions

--- src/Parser/Generator/Util.hs
+++ src/Parser/Generator/Util.hs
@@ -8,12 +8,14 @@ import Parser.Data.ParseTree
 gTerm            :: ParseTree -> String
 gTerm (Token tk) = tk
 
--- The first equation of the following functions unpack the top level rule and
--- it is necessary because of the recursive repetition pattern describe by them
-gOpt                       :: (ParseTree -> String) -> ParseTree -> String
-gOpt g (Rule "Optional" t) = case t of
-    Eps -> ""
-    _   -> g t
+-- The first equation of the following functions (Rule ...) unpack the actual
+-- structure of the tree and it is necessary because of the recursive pattern
+-- described by them
+gOpt     :: (ParseTree -> String) -> ParseTree -> String
+gOpt g t = case t of
+    (Rule "Optional" t') -> gOpt g t'
+    Eps                  -> ""
+    _                    -> g t
 
 gClojure     :: (ParseTree -> String) -> ParseTree -> String
 gClojure g t = case t of
