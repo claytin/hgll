a1f469a7fdc6a1238e65e284242b9ab5165f7d4c
claytown <>
2019-06-08 16:53:52 -0300

    Implementation now uses Parser.Instance.Std; better comments

--- src/Parser/Combinators/Base.hs
+++ src/Parser/Combinators/Base.hs
@@ -3,97 +3,72 @@ module Parser.Combinators.Base ( parse
                                , failure
                                , eps
                                , term, t
-                               , sqnc, (+>)
+                               , sqnc, (#)
                                , rule, (=|>) ) where
 
-import Parser.Types
+import Parser.Instance.Std
+import Parser.Data.ParseTree
 
--- The parse function deconstructs and applies a parser
-parse            :: Parser a -> (Input -> [Res a])
-parse (Parser p) = p
-
--- Combinators --
+-- Standard Combinators --
 -- A parser that always succeeds for a given value
-success   :: a -> Parser a
--- success v = memo $ Parser $ \i m -> (Right (v, i), m)
-success v = Parser $ \i -> [(v, i)]
+success :: a -> Std a
+success = return
 
 -- The complement of success, it always fails
-failure :: Parser a
-failure = Parser $ \i -> [ ]
---failure = memo $ Parser $ \i m -> (Left i, m)
+failure :: Std a
+failure = Std $ \_ -> [ ]
 
 -- Parser that represents an empty production. The parser always succeeds with
 -- an "empty" ParseTree (see Parser.Data.ParseTree)
-eps :: Parser ParseTree
+eps :: Std ParseTree
 eps = success Eps
 
 -- This is the only parser for matching terminals. It takes a string s that
 -- will be matched against the first n characters of input i, where n is the
 -- length of the parameter s. In case of match the Token s (or s') is returned.
--- Unlike most common parser combinators no parametrization of the token
--- content (value) is allowed. ISO EBNF standart defines strings as the only
--- terminal elements, therefore is not possible to distinguish types on a
--- grammar definition
-term    :: String -> Parser ParseTree
+-- Unlike most common parser combinators, no parametrization of the token
+-- content (value) is allowed. ISO EBNF standard defines strings as the only
+-- terminal elements
+term    :: String -> Std ParseTree
 term "" = error "Terminals must be non empty strings!"
-term s  = Parser $ \i ->
+term s  = Std $ \i ->
     let n  = length s
         s' = take n i
         i' = drop n i
-     in if s == s' then [(Token s', i')]
+     in if s == s' then [(i', Token s')]
                    else [ ]
 
--- Altough it returns a parser, i see this as more of a auxilarie function,
--- very convinient for the implementation of sequencies and recursive patterns.
--- It takes a parser and a function that returns a parser. The function f takes
--- the value returned by the parser p and carries it (binds) forward into its
--- "scope". If any of the parsers, p or the one returned by f, fail, then bind
--- also fails
-bind     :: Parser a -> (a -> Parser b) -> Parser b
-bind p f = Parser $ \i -> concat [ parse (f v) i' | (v, i') <- parse p i ]
-
 -- The sequence combinator is a function that takes two parsers and apply both
--- of them, in order, from the left, to the given input by way of the bind
--- "parser".
-sqnc     :: Parser ParseTree -> Parser ParseTree -> Parser ParseTree
-sqnc p q = p `bind` \x ->
-           q `bind` \y ->
-           success $ Seq x y
+-- of them, in order, from the left, to the given input
+sqnc     :: Std ParseTree -> Std ParseTree -> Std ParseTree
+sqnc p q = p <:> q >>= \(x, y) -> return $ Seq x y
 
--- A helper function that takes the result of the application of a parser and
--- "encapsulates" it into a new result. This new result implies no change to
--- the result of the computation. If the original result was a failure, label
--- behaves like the identity function. Otherwise, it will take the resulting
--- parse tree and "put it inside" a new Rule parse tree labeled by l. Every
--- element of a grammar (except maybe for the start rule) must be an element
--- of some rule, the label function just names what rule it is
-label               :: Label -> [(ParseTree, Input)] -> [(ParseTree, Input)]
-label _ [ ]         = [ ]
-label l ((t, i):xs) = (Rule l t, i):label l xs
 
--- A first match alternative combinator, the first of the alternatives to
--- succeed is returned. It fails if none of the alternatives can parse the
--- input
-rule        :: Label -> [Parser ParseTree] -> Parser ParseTree
-rule l []   = failure -- a rule must not be empty
-rule l alts = Parser $ \i -> label l $ parse (all alts) i
+-- The rule function is more of a wrapper function. It takes a label, which
+-- will identify a set of alternatives alts. Even though alts is a single
+-- parser it is expected for it to be compose by one or more parsers combined
+-- by the alternative operator (<|>), see Parser.Intance.Std
+rule        :: Label -> Std ParseTree -> Std ParseTree
+rule l alts = Std $ \i -> label l $ parse alts i
     where
-        -- all reduces the list of parsers by means of plus
-        all = foldl1 (plus)
-        -- plus takes two parsers cur and nxt, and returns a new parser that
-        -- combines the application of both, cur and nxt
-        cur `plus` nxt = Parser $ \i -> parse cur i ++ parse nxt i
+        -- The function label takes the result of the application of a parser
+        -- and "encapsulates" it into a new result. If the original result was
+        -- a failure, label behaves like the identity function. Otherwise, it
+        -- will take the resulting parse tree t and "put it inside" a new Rule
+        -- parse tree labeled by l. No actual change to the structure of t
+        -- occurs
+        -- label :: String -> [Res ParseTree] -> [Res ParseTree]
+        label l rs = [ (i', Rule l t) | (i', t) <- rs ]
 
 -- Aliases --
 t = term
 
--- If (+>) is applied to a sequence of, say, elements a, b, and c a +> b +> c
--- is equivalent to (a +> b) +> c
-infixl 1 +>
-(+>) = sqnc
+-- If (#) is applied to a sequence of, say, elements a, b, and c, a # b # c
+-- is equivalent to (a # b) # c
+infixl 3 #
+(#) = sqnc
 
--- This fixity definition of rule is as follows so that it stays close to
--- its function form
-infix 9 =|>
+-- This fixity definition of rule is as follows so that it will be the last
+-- element to be resolved from the definition of a grammar rule
+infix 1 =|>
 (=|>) = rule
--- src/Parser/Combinators/Ext.hs
+++ src/Parser/Combinators/Ext.hs
@@ -1,44 +1,54 @@
--- TODO!
--- 1. Add some comments
 module Parser.Combinators.Ext ( module Parser.Combinators.Base
                               , rule', (=!>)
                               , opt
                               , clojure, star
                               , times, (*.) ) where
 
-import Parser.Types
+import Parser.Instance.Std
+import Parser.Data.ParseTree
+
 import Parser.Combinators.Base
 
-rule'        :: Label -> [Parser ParseTree] -> Parser ParseTree
-rule' l []   = failure -- a rule must not be empty
-rule' l alts = Parser $ \i ->
+-- The rule' is a firs match variation of rule. It is quite convenient on the
+-- definition of certain patterns/combinators such as the ones bellow, where
+-- an ambiguous combinator suchs as rule will return every intermidiate result
+-- of the parser application, when we are only interested in the longest match,
+-- which happens to be the first element of the list of results
+rule'        :: Label -> Std ParseTree -> Std ParseTree
+rule' l alts = Std $ \i ->
     case parse (rule l alts) i of
         [ ]    -> [ ]
         (x:xs) -> [x]
 
-opt   :: Parser ParseTree -> Parser ParseTree
-opt p = rule' "Optional" [ p , eps ]
+-- A parser that always succeeds, either by the success of its parameter p, or
+-- by the empty production. This represents a sequence of at most one match of
+-- the pattern described by p
+opt   :: Std ParseTree -> Std ParseTree
+opt p = "Optional" =!> p <|> eps
 
-clojure   :: Parser ParseTree -> Parser ParseTree
-clojure p = rule' "Clojure" [ p', eps ]
+-- Similar to opt, except that clojure represents a unbount sequence of matches
+-- of the pattern described by p
+clojure   :: Std ParseTree -> Std ParseTree
+clojure p = "Clojure" =!> p' <|> eps
     where
-        p' = p +> clojure p
+        p' = p # clojure p
 
-times     :: Int -> Parser ParseTree -> Parser ParseTree
+-- The times combinator defines a finite sequence, of exactly n, matches of the
+-- pattern described by p
+times     :: Int -> Std ParseTree -> Std ParseTree
 times 0 _ = eps
 times 1 p = p
-times n p = rule' "Repetition" [ p' ]
+times n p = "Repetition" =!> p'
     where
-        p' = p +> (n - 1) `times` p
+        p' = p # (n - 1) *. p
 
 -- Aliases --
 star = clojure
 
--- The precedence is set to zero beacause the precedence of (+>, sequence) is
--- set to one, repetition must have a higher precedence
-infixl 2 *.
+-- The highest precedence on the set of EBNF operators
+infixl 4 *.
 (*.) = times
 
--- See rule fixity definition in Parser.Combinators.Base
-infix 9 =!>
+-- See the rule combinator fixity definition in Parser.Combinators.Base
+infix 1 =!>
 (=!>) = rule'
