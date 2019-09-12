cc39f93a0986b4dd0bcfca635e8619b8fb4ac721
claytown <>
2019-09-11 22:07:34 -0300

    Changed the organization to better fit the needs of latex; cryptic commit I know

--- src/Parser/Generator/Words.hs
+++ src/Parser/Generator/Words.hs
@@ -3,55 +3,59 @@ module Parser.Generator.Words ( gTerminalString
                               , gInteger ) where
 
 import Parser.Data.ParseTree
-import Parser.Generator.Util (gStar)
 
+import Parser.Generator.Util
 import Parser.Generator.CharSet
 
 gTerminalString (Rule "TerminalString" t) = case t of
-    (Seq (Seq (Seq a@(Rule "SingleQuoteSymbol"_) b) c) e) ->
-        "t " ++ gSingleQuoteSymbol a
-             ++ gFirstTerminalCharacter b
-             ++ gStar gFirstTerminalCharacter c
-             ++ gSingleQuoteSymbol e
-    (Seq (Seq (Seq a@(Rule "DoubleQuoteSymbol"_) b) c) e) ->
-        "t " ++ gDoubleQuoteSymbol a
-             ++ gSecondTerminalCharacter b
-             ++ gStar gSecondTerminalCharacter c
-             ++ gDoubleQuoteSymbol e
+    (Seq (Seq (Seq (Rule "FirstQuoteSymbol" _) b) c) _) ->
+        cTermOp ++ cQuoteSym
+                ++ gFirstTerminalCharacter b
+                ++ gStar gFirstTerminalCharacter c
+                ++ cQuoteSym
+    (Seq (Seq (Seq (Rule "SecondQuoteSymbol" _) b) c) _) ->
+        cTermOp ++ cQuoteSym
+                ++ gSecondTerminalCharacter b
+                ++ gStar gSecondTerminalCharacter c
+                ++ cQuoteSym
 
 gFirstTerminalCharacter (Rule "FirstTerminalCharacter" t) = case t of
-    (Rule "Letter" _)            -> gLetter t
-    (Rule "DecimalDigit" _)      -> gDecimalDigit t
-    (Rule "ConcatenateSymbol" _) -> gConcatenateSymbol t
-    (Rule "AlternativeSymbol" _) -> gAlternativeSymbol t
-    (Rule "DefiningSymbol" _)    -> gDefiningSymbol t
-    (Rule "StartGroupSymbol" _)  -> gStartGroupSymbol t
-    (Rule "StartOptionSymbol" _) -> gStartOptionSymbol t
-    (Rule "StartRepeatSymbol" _) -> gStartRepeatSymbol t
-    (Rule "EndGroupSymbol" _)    -> gEndGroupSymbol t
-    (Rule "EndOptionSymbol" _)   -> gEndOptionSymbol t
-    (Rule "EndRepeatSymbol" _)   -> gEndRepeatSymbol t
-    (Rule "DoubleQuoteSymbol" _) -> gDoubleQuoteSymbol t
-    (Rule "RepetitionSymbol" _)  -> gRepetitionSymbol t
-    (Rule "TerminatorSymbol" _)  -> gTerminatorSymbol t
-    (Rule "OtherCharacter" _)    -> gOtherCharacter t
+    (Rule "Letter" _)                -> gLetter t
+    (Rule "DecimalDigit" _)          -> gDecimalDigit t
+    (Rule "ConcatenateSymbol" _)     -> gConcatenateSymbol t
+    (Rule "DefiningSymbol" _)        -> gDefiningSymbol t
+    (Rule "AlternativeSymbol" _)     -> gAlternativeSymbol t
+    (Rule "EndGroupSymbol" _)        -> gEndGroupSymbol t
+    (Rule "EndOptionSymbol" _)       -> gEndOptionSymbol t
+    (Rule "EndRepeatSymbol" _)       -> gEndRepeatSymbol t
+    (Rule "ExceptSymbol" _)          -> gExceptSymbol t
+    (Rule "RepetitionSymbol" _)      -> gRepetitionSymbol t
+    (Rule "SecondQuoteSymbol" _)     -> gSecondQuoteSymbol t
+    (Rule "SpecialSequenceSymbol" _) -> gSpecialSequenceSymbol t
+    (Rule "StartGroupSymbol" _)      -> gStartGroupSymbol t
+    (Rule "StartOptionSymbol" _)     -> gStartOptionSymbol t
+    (Rule "StartRepeatSymbol" _)     -> gStartRepeatSymbol t
+    (Rule "TerminatorSymbol" _)      -> gTerminatorSymbol t
+    (Rule "OtherCharacter" _)        -> gOtherCharacter t
 
 gSecondTerminalCharacter (Rule "SecondTerminalCharacter" t) = case t of
-    (Rule "Letter" _)            -> gLetter t
-    (Rule "DecimalDigit" _)      -> gDecimalDigit t
-    (Rule "ConcatenateSymbol" _) -> gConcatenateSymbol t
-    (Rule "AlternativeSymbol" _) -> gAlternativeSymbol t
-    (Rule "DefiningSymbol" _)    -> gDefiningSymbol t
-    (Rule "StartGroupSymbol" _)  -> gStartGroupSymbol t
-    (Rule "StartOptionSymbol" _) -> gStartOptionSymbol t
-    (Rule "StartRepeatSymbol" _) -> gStartRepeatSymbol t
-    (Rule "EndGroupSymbol" _)    -> gEndGroupSymbol t
-    (Rule "EndOptionSymbol" _)   -> gEndOptionSymbol t
-    (Rule "EndRepeatSymbol" _)   -> gEndRepeatSymbol t
-    (Rule "SingleQuoteSymbol" _) -> gSingleQuoteSymbol t
-    (Rule "RepetitionSymbol" _)  -> gRepetitionSymbol t
-    (Rule "TerminatorSymbol" _)  -> gTerminatorSymbol t
-    (Rule "OtherCharacter" _)    -> gOtherCharacter t
+    (Rule "Letter" _)                -> gLetter t
+    (Rule "DecimalDigit" _)          -> gDecimalDigit t
+    (Rule "ConcatenateSymbol" _)     -> gConcatenateSymbol t
+    (Rule "DefiningSymbol" _)        -> gDefiningSymbol t
+    (Rule "AlternativeSymbol" _)     -> gAlternativeSymbol t
+    (Rule "EndGroupSymbol" _)        -> gEndGroupSymbol t
+    (Rule "EndOptionSymbol" _)       -> gEndOptionSymbol t
+    (Rule "EndRepeatSymbol" _)       -> gEndRepeatSymbol t
+    (Rule "ExceptSymbol" _)          -> gExceptSymbol t
+    (Rule "FirstQuoteSymbol" _)      -> gFirstQuoteSymbol t
+    (Rule "RepetitionSymbol" _)      -> gRepetitionSymbol t
+    (Rule "SpecialSequenceSymbol" _) -> gSpecialSequenceSymbol t
+    (Rule "StartGroupSymbol" _)      -> gStartGroupSymbol t
+    (Rule "StartOptionSymbol" _)     -> gStartOptionSymbol t
+    (Rule "StartRepeatSymbol" _)     -> gStartRepeatSymbol t
+    (Rule "TerminatorSymbol" _)      -> gTerminatorSymbol t
+    (Rule "OtherCharacter" _)        -> gOtherCharacter t
 
 gMetaIdentifier (Rule "MetaIdentifier" t) = case t of
     (Seq l r) -> gLetter l ++ gStar gMetaIdentifierCharacter r
