52a211a261d446fa3f6df52e99a679759e0f10f6
claytown <>
2019-09-11 22:11:44 -0300

    Changed the notation for the result returned by parser p in the function fullParse

--- src/Parser/Traits.hs
+++ src/Parser/Traits.hs
@@ -3,8 +3,9 @@ module Parser.Traits where
 class Parser m where
     parse         :: m a -> String -> [(String, a)]
     fullParse     :: m a -> String -> [a]
-    fullParse p i = [ a | ([], a) <- parse p i ]
+    fullParse p i = [ a | ("", a) <- parse p i ]
 
+-- Add a restriction to GrammO?
 class GrammO m where
     (<:>) :: m a -> m b -> m (a, b)
     (<|>) :: m a -> m a -> m a
