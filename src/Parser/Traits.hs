ed04533138ffe5419ef98e4623b5cfbd4fad80a0
claytin <>
2019-10-25 12:30:29 -0300

    Renamed operator <:> as <,>

--- src/Parser/Traits.hs
+++ src/Parser/Traits.hs
@@ -5,10 +5,9 @@ class Parser m where
     fullParse     :: m a -> String -> [a]
     fullParse p i = [ a | ("", a) <- parse p i ]
 
--- Add a restriction to GrammO?
 class GrammO m where
-    (<:>) :: m a -> m b -> m (a, b)
+    (<,>) :: m a -> m b -> m (a, b)
     (<|>) :: m a -> m a -> m a
 
-infixl 3 <:>
+infixl 3 <,>
 infixl 2 <|>
