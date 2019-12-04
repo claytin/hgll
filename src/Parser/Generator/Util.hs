57caa389eec192e05ea35ba53d1342092bdc0a09
claytin <>
2019-12-04 17:11:52 -0300

    Only functions for generation of "special" rules remain; Update of nomenclature

--- src/Parser/Generator/Util.hs
+++ src/Parser/Generator/Util.hs
@@ -1,79 +1,43 @@
 module Parser.Generator.Util ( gTerm
-                             , gOpt
-                             , gClosure, gStar
-                             , gTimes
-                             , cQuoteSym
-                             , cOSSeqSym
-                             , cCSSeqSym
-                             , cTermOp
-                             , cAltOp
-                             , cSeqOp
-                             , cRuleDefOp
-                             , cRuleTermSym
-                             , ecRepOp
-                             , ecOptOp
-                             , ecClosureOp ) where
+                             , gOptional
+                             , gClosure
+                             , gRepetition
+                             , module Parser.Generator.Util.C
+                             , module Parser.Generator.Util.S ) where
 
 import Parser.Data.ParseTree
 
-gTerm            :: ParseTree -> String
+import Parser.Generator.Util.C
+import Parser.Generator.Util.S
+
+-- This is an auxilary function for extracting the terminal string tk from the
+-- terminal tree Token. The function gTerm is only used to generate terminals,
+-- meaning only elements of final strings;
+gTerm :: ParseTree -> String
 gTerm (Token tk) = tk
 
--- The first equation of the following functions (Rule ...) unpack the actual
--- structure of the tree and it is necessary because of the recursive pattern
--- described by them
-gOpt     :: (ParseTree -> String) -> ParseTree -> String
-gOpt g t = case t of
-    (Rule "Optional" t') -> gOpt g t'
-    Eps                  -> ""
-    _                    -> g t
+-- The first equation in the following functions (Rule ...) unpacks the actual
+-- tree structure and is necessary because of the recursive pattern described
+-- by them;
+-- For a better understanding of how these functions work and of their pattern
+-- matching see Parser/Combinators/Ext*.hs
+gOptional     :: (ParseTree -> String) -> ParseTree -> String
+gOptional g t = case t of
+    (Rule "Optional" t') -> gOptional g t'
+    -- this comment creates separation from the above "dummy" equation
+    Eps -> gEpsS
+    _   -> g t
 
 gClosure     :: (ParseTree -> String) -> ParseTree -> String
 gClosure g t = case t of
     (Rule "Closure" t') -> gClosure g t'
-    (Seq l r)           -> g l ++ gClosure g r
-    Eps                 -> ""
+    --
+    (Seq l r) -> g l ++ gClosure g r
+    Eps       -> gEpsS
 
-gTimes       :: Int -> (ParseTree -> String) -> ParseTree -> String
-gTimes 0 _ _ = ""
-gTimes 1 g t = g t
-gTimes n g t = case t of
-    (Rule "Repetition" t') -> gTimes n g t'
-    (Seq l r)              -> g l ++ gTimes (n - 1) g r
-
--- Combinator generation operators and symbols
-cQuoteSym :: String
-cQuoteSym = "\""
-
-cOSSeqSym :: String
-cOSSeqSym = "("
-
-cCSSeqSym :: String
-cCSSeqSym = ")"
-
-cTermOp :: String
-cTermOp = "t "
-
-cAltOp :: String
-cAltOp = " <|> "
-
-cSeqOp :: String
-cSeqOp = " # "
-
-cRuleDefOp :: String
-cRuleDefOp = " =|> "
-
-cRuleTermSym :: String
-cRuleTermSym = "\n"
-
-ecRepOp :: String
-ecRepOp = " *. "
-
-ecOptOp :: String
-ecOptOp = "opt "
-
-ecClosureOp :: String
-ecClosureOp = "closure "
-
--- Aliases --
-gStar = gClosure
+gRepetition       :: Int -> (ParseTree -> String) -> ParseTree -> String
+gRepetition 0 _ t = gEpsS
+gRepetition n g t = case t of
+    (Rule "Repetition" t') -> gRepetition n g t'
+    --
+    (Seq l r) -> g l ++ gRepetition (n - 1) g r
