3ba9144dee4028c0c72e2987b9751033b55ede3c
claytin <>
2019-12-09 14:39:43 -0300

    Introduced the syntacticTerm rule; Updated the singleDefinition rule, it is now defined in terms of syntacticTerm

--- src/Parser/EBNF/Syntax.hs
+++ src/Parser/EBNF/Syntax.hs
@@ -13,18 +13,15 @@ syntaxRule = "SyntaxRule" =!>
 definitionsList = "DefinitionsList" =!>
     singleDefinition # star (alternativeSymbol # singleDefinition)
 
--- In the definition of the rule SingleDefinition syntacticTerm should replace
--- syntacticFactor, but the first was removed because rules based on set
--- diferences, described by syntacticTerm, are not supported
 singleDefinition = "SingleDefinition" =!>
-     syntacticFactor # star (concatenateSymbol # syntacticFactor)
+     syntacticTerm # star (concatenateSymbol # syntacticTerm)
+
+syntacticTerm = "SyntacticTerm" =!>
+    syntacticFactor # opt (exceptSymbol # syntacticFactor)
 
 syntacticFactor = "SyntacticFactor" =!>
      opt (integer # repetitionSymbol) # syntacticPrimary
 
--- Note: the non-terminal emptySequence must come before metaIdentifier, since
--- the first is a meta identifier, but a special one, and it must be recognized
--- as such
 syntacticPrimary = "SyntacticPrimary" =!>
                     optionalSequence
                 <|> repeatedSequence
@@ -42,7 +39,4 @@ repeatedSequence = "RepeatedSequence" =!>
 groupedSequence = "GroupedSequence" =!>
     startGroupSymbol # definitionsList # endGroupSymbol
 
--- This does not correspond to the ISO EBNF standard, but it is necessary
--- because it is "not possible" to distinguish an empty production otherwise
--- emptySequence = "EmptySequence" =!> t "eps"
 emptySequence = "EmptySequence" =!> eps
